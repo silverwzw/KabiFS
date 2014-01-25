c2c5f78632bcedd8bcc500c5b09b02fafda47d9c
silverwzw <git@silverwzw.com>
2014-01-25 14:39:49 -0500

    rename c units & build java env

---
+++ src/com/silverwzw/kabiFS/KabiFS.java
@@ -0,0 +1,12 @@
+package com.silverwzw.kabiFS;
+
+import com.sun.jna.Callback;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
+import org.apache.log4j.Logger; 
+
+public class KabiFS {
+	;
+}
